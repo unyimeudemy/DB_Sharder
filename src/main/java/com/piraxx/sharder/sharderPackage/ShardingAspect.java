bc38beb42b8b1781e32892eea8c3ad34cb2634db
unyimeudemy <unyimeudemy20@gmail.com>
2024-09-16 14:18:45 +0100

    chore: sharding aspect class under improvement

--- src/main/java/com/piraxx/sharder/sharderPackage/ShardingAspect.java
+++ src/main/java/com/piraxx/sharder/sharderPackage/ShardingAspect.java
@@ -1,6 +1,7 @@
 package com.piraxx.sharder.sharderPackage;
 
 import com.piraxx.sharder.domain.TransactionEntity;
+import jakarta.persistence.Entity;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
@@ -18,12 +19,22 @@ public class ShardingAspect {
     private void shardingAspect(JoinPoint joinPoint){
         Object[] args = joinPoint.getArgs();
 
-        for (Object arg: args){
-            if(arg instanceof TransactionEntity){
-                TransactionEntity transactionEntity = (TransactionEntity) arg;
-                String shardKey = determineShard(transactionEntity.getTransactionId());
-                ShardingContextHolder.setCurrentShardKey(shardKey);
+        if(args.length == 0){
+            // if request comes without arg, like findAll
+
+
+        }else if(args.length == 1){
+            // if request comes in with only one arg like findById or save etc
+            Object arg = args[0];
+            if(arg instanceof String || arg instanceof  Number){
+                // if the arg is just an ID like UUID or from snowflake like in findById etc
+                simpleSelectShardSingleItem(arg);
+            }else{
+                // if the arg is an entity like in save
+                simpleSelectShardSingleItem(arg);
             }
+        }else {
+            simpleSelectShardArr(args);
         }
     }
 
@@ -33,11 +44,51 @@ public class ShardingAspect {
         System.out.println("Sharding context cleared");
     }
 
-    private static String determineShard(Integer transactionId){
-        if(transactionId % 2 == 0){
+    private static void simpleSelectShardArr(Object[] args){
+        for (Object arg: args){
+            if(arg instanceof TransactionEntity){
+                TransactionEntity transactionEntity = (TransactionEntity) arg;
+                String shardKey = determineShard(transactionEntity.getTransactionId());
+                ShardingContextHolder.setCurrentShardKey(shardKey);
+            }
+        }
+    }
+
+    private static void simpleSelectShardSingleItem(Object arg){
+
+            if(arg instanceof Entity){
+                //argument is an entity
+
+                Object entity = entityList(arg);
+                TransactionEntity transactionEntity = (TransactionEntity) arg;
+                String shardKey = determineShard(transactionEntity.getTransactionId());
+                ShardingContextHolder.setCurrentShardKey(shardKey);
+
+            }else{
+                // if argument is a string or a number
+                String shardKey = determineShard(arg);
+                ShardingContextHolder.setCurrentShardKey(shardKey);
+            }
+    }
+
+    private static Object entityList(Object arg){
+        // scans through a
+    }
+
+    private static getIdField(){
+
+    }
+
+
+
+
+    private static String determineShard(Object obj){
+        int id = obj.hashCode();
+        if(id % 2 == 0){
             return "shard1";
         }else{
             return "shard2";
         }
     }
+
 }
